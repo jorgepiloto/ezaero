18b5fbda394e11c389a1c4f9978e863160ed6398
Jorge M.G <jorge@on-orbit.dev>
2020-10-23 19:44:10 +0200

    Compute panels circulation

--- src/ezaero/vlm/steady.py
+++ src/ezaero/vlm/steady.py
@@ -14,13 +14,12 @@ import numpy as np
 import plotly.graph_objects as go
 
 from ezaero.vlm.meshing import mesh_surface
-from ezaero.vlm.utils import biot_savart
-
 from ezaero.vlm.plotting import (
     plot_cl_distribution_on_wing,
     plot_control_points,
     plot_panels,
 )
+from ezaero.vlm.utils import biot_savart
 
 
 class VLM_solver:
@@ -82,6 +81,25 @@ class VLM_solver:
         for panels, cpoints in zip(self.all_panels, self.all_cpoints):
             fig = plot_panels(panels, fig=fig)
             fig = plot_control_points(cpoints, fig=fig)
+
+        return fig
+
+    def plot_wake(self, fig=None, color="red"):
+
+        if not fig:
+            fig = go.Figure()
+            fig.update_layout(scene_aspectmode="data")
+
+        for part_wakes in self.all_wakes:
+            for r_wake in part_wakes:
+                wake_trace = go.Scatter3d(
+                    x=list(r_wake[:, 0]),
+                    y=list(r_wake[:, 1]),
+                    z=list(r_wake[:, 2]),
+                    showlegend=False,
+                    marker=dict(color=color, size=5),
+                )
+                fig.add_trace(wake_trace)
 
         return fig
 
@@ -179,7 +197,7 @@ class VLM_solver:
 
             self.all_surfaces.append(panel_surfaces)
 
-    def _build_wake(self, offset=300):
+    def _build_wake(self, offset=2):
         """
         Build the steady wake vortex panels.
 
@@ -226,21 +244,22 @@ class VLM_solver:
         """
 
         # Number of panels
-        self.N_panels = np.sum([panels.shape[0] * panels.shape[1] for panels in self.all_panels])
+        self.N_panels = np.sum(
+            [panels.shape[0] * panels.shape[1] for panels in self.all_panels]
+        )
 
         # Convert to array
         cpoints = np.asarray(self.all_cpoints)
         vortex_panels = np.asarray(self.all_vortex_panels)
         normals = np.asarray(self.all_normals)
 
-
-        r = vortex_panels.reshape(
-            (self.N_panels, 1, 4, 3)
-        ) - cpoints.reshape((1, self.N_panels, 1, 3))
+        r = vortex_panels.reshape((self.N_panels, 1, 4, 3)) - cpoints.reshape(
+            (1, self.N_panels, 1, 3)
+        )
 
         vel = biot_savart(r)
         nv = normals.reshape((self.N_panels, 3))
-        self.aic_wing = np.einsum("ijk,jk->ji", vel, nv)
+        self.all_aic_wing = np.einsum("ijk,jk->ji", vel, nv)
 
     def _calculate_wake_wing_influence_matrix(self):
         """
@@ -262,19 +281,21 @@ class VLM_solver:
             cpoints = np.asarray(self.all_cpoints)[i]
             wake = self.all_wakes[i]
             normals = np.asarray(self.all_normals)[i]
-            m, n= np.asarray(self.all_meshconf)[i,:]
+            m, n = np.asarray(self.all_meshconf)[i, :]
+            mn = m * n
 
-            r = wake[:, np.newaxis, :, :] - cpoints.reshape((1, m*n, 1, 3))
+            r = wake[:, np.newaxis, :, :] - cpoints.reshape((1, mn, 1, 3))
             vel = biot_savart(r)
-            nv = normals.reshape((m * n, 3))
+            nv = normals.reshape((mn, 3))
 
-            aic_wake = np.zeros((m * n, m * n))
+            aic_wake = np.zeros((mn, mn))
 
-            aic_wake[:, -n :] = np.einsum("ijk,jk->ji", vel, nv)
-            print(aic_wake[0,:])
-            #self.all_aic_wake = np.append(self.all_aic_wake, aic_wake)
+            aic_wake[:, -n:] = np.einsum("ijk,jk->ji", vel, nv)
+            lim, _ = aic_wake.shape
 
-
+            self.all_aic_wake[
+                i * lim : (i + 1) * lim, i * lim : (i + 1) * lim
+            ] = aic_wake
 
     def _calculate_influence_matrix(self):
         """
@@ -285,7 +306,7 @@ class VLM_solver:
         """
         self._calculate_wing_influence_matrix()
         self._calculate_wake_wing_influence_matrix()
-        self.aic = self.aic_wing + self.aic_wake
+        self.all_aic = self.all_aic_wing + self.all_aic_wake
 
     def _calculate_rhs(self):
         """
@@ -294,10 +315,11 @@ class VLM_solver:
         rhs : np.ndarray, shape (m * n, )
             RHS vector.
         """
-        u = self.flight_conditions.ui * np.array(
-            [np.cos(self.flight_conditions.aoa), 0, np.sin(self.flight_conditions.aoa)]
-        )
-        self.rhs = -np.dot(self.normals.reshape(self.mesh.m * self.mesh.n, -1), u)
+
+        normals = np.asarray(self.all_normals)
+
+        u = self.Uinf * np.array([np.cos(self.alpha), 0, np.sin(self.alpha)])
+        self.rhs = -np.dot(normals.reshape(self.N_panels, -1), u)
 
     def _solve_net_panel_circulation_distribution(self):
         """
@@ -309,17 +331,29 @@ class VLM_solver:
         net_circulation : np.ndarray, shape (m, n)
             Array containing net circulation for each panel.
         """
-        g = np.linalg.solve(self.aic, self.rhs).reshape(self.mesh.m, self.mesh.n)
 
-        self.net_circulation = np.empty_like(g)
-        self.net_circulation[0, :] = g[0, :]
-        self.net_circulation[1:, :] = g[1:, :] - g[:-1, :]
+        # Solve whole solution
+        g = np.linalg.solve(self.all_aic, self.rhs)
+
+        # Allocate panels gamma solutions
+        self.all_circulations = []
+
+        # Deconstruct matrix solution
+        for (m, n) in self.all_meshconf:
+
+            # Total number of panels for the part
+            mn = m * n
+
+            # Get the first elements of circulation solution and remove them
+            panel_gamma = g[:mn].reshape(m, n)
+            self.all_circulations.append(panel_gamma)
+            g = g[mn:]
 
     def _calculate_aero_distributions_from_circulation(self):
         m, n = self.mesh.m, self.mesh.n
         rho, ui = (
-            self.flight_conditions.rho,
-            self.flight_conditions.ui,
+            self.rho,
+            self.Uinf,
         )
         bp = self.wing.planform_wingspan
         dL = self.net_circulation * rho * ui * bp / n
@@ -346,3 +380,6 @@ class VLM_solver:
             self._build_wake()
             self._calculate_wing_influence_matrix()
             self._calculate_wake_wing_influence_matrix()
+            self._calculate_influence_matrix()
+            self._calculate_rhs()
+            self._solve_net_panel_circulation_distribution()
